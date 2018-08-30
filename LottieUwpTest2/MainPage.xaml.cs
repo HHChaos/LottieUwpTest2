eaebd007e603c64b524c82a150a74333094968a7
hhchaos <HHChaos@outlook.com>
2018-08-30 10:55:14 +0800

    添加项目文件。

---
+++ LottieUwpTest2/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using LottieUWP;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace LottieUwpTest2
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var localDir = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets");
+            var file = await localDir.GetFileAsync("test.json");
+            var json = await FileIO.ReadTextAsync(file, Windows.Storage.Streams.UnicodeEncoding.Utf8);
+
+            await LottieView.SetAnimationFromJsonAsync(json, "test");
+            var compositionResult = await LottieCompositionFactory.FromJsonString(json,"test");
+
+            if (compositionResult.Value != null)
+            {
+                LottieDrawable.SetComposition(compositionResult.Value);
+                LottieDrawable.RepeatCount = -1;
+                LottieDrawable.PlayAnimation();
+            }
+
+            LottieView.PlayAnimation();
+        }
+    }
+}
